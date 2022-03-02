966e48143b6f741c872a08e32f45da5801eda9d9
Marco Ramos <majramos@gmail.com>
2022-03-02 22:16:03 +0000

    rename file due to location and add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "installing conky configuration"
+
+# make folder to store configuration
+mkdir ~/.config/conky
+
+# set autostart option
+cp conky.desktop ~/.config/autostart
+
+
+echo "conky installed!"
